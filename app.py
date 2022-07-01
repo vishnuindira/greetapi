dddf5900ad5703a2e86a311d0e0742287b1e418d
Vishnu prasad <vishnuindira123@gmail.com>
2022-07-01 09:45:13 +0530

    create app.py for bootcamp

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
+from flask import Flask, jsonify
+app = Flask(__name__)
+@app.route('/name')
+def index():
+    return jsonify({'name': 'vishnu',
+                    'email': 'vishnu@outlook.com'})
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+app.run()
+
